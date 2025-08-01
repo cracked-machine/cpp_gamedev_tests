d065af67335b820a07419288c71de29f7e9e52f6
cracked-machine <chrisjsutton@gmail.com>
2025-08-01 01:09:06 +0000

    added compile commands output for clangd

--- sfml3/build.sh
+++ sfml3/build.sh
@@ -2,7 +2,8 @@
 
 cd sfml3
 cmake -S . -B build -G Ninja \
-    -DCMAKE_TOOLCHAIN_FILE=cmake-toolchain-mingw64-x86_64.cmake
+    -DCMAKE_TOOLCHAIN_FILE=cmake-toolchain-mingw64-x86_64.cmake \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build build --parallel
 
 cp /usr/lib/gcc/x86_64-w64-mingw32/14-win32/libstdc++-6.dll build/bin/libstdc++-6.dll
