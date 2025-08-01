65079829b5217c9f74134b61e850055a45989a39
cracked-machine <chrisjsutton@gmail.com>
2025-08-01 01:10:23 +0000

    added example builds and installed precompiled runtime libs

--- sfml3/build.sh
+++ sfml3/build.sh
@@ -6,5 +6,4 @@ cmake -S . -B build -G Ninja \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build build --parallel
 
-cp /usr/lib/gcc/x86_64-w64-mingw32/14-win32/libstdc++-6.dll build/bin/libstdc++-6.dll
-cp /usr/lib/gcc/x86_64-w64-mingw32/14-win32/libgcc_s_seh-1.dll build/bin/libgcc_s_seh-1.dll
+chmod 777 build/bin/*
